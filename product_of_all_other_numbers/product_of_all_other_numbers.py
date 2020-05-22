ca499c971556a5f753b1b57f965d3a446f5fd5a5
Dani Blackwell <bleuxgenius@gmail.com>
2020-05-21 23:13:56 -0500

    completing first day MVP

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,38 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # #iterate over the list of integers
+    # multip all numbers together 
+    # if number is a zero at each index divide the value from total 
 
-    pass
+    firstNumber = 1 
+    zeroNumber = 1 
+
+    for value in arr:
+        # if value is not equal to 0
+        if value != 0:
+            # first number is multiplied and equal to the value 
+            firstNumber *= value
+              # and the zero number is multiplied and equal to the value 
+            zeroNumber *= value
+        else:
+            # or else the first number is multiplied and equal to the value 
+            firstNumber *= value
+            # and the zero number is multiplied and equal to 1 
+            zeroNumber *= 1
+
+# the new array will be an empty array 
+    new_arr = []
+
+# create the next for loop 
+    for i in arr:
+        if i == 0:
+            new_arr.append(zeroNumber)
+        else:
+            divide = firstNumber // i
+            new_arr.append(divide)      
+
+    return new_arr                  
 
 
 if __name__ == '__main__':
